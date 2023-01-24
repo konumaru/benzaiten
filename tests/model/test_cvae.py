75a1c77b6dc029900e6db94cccef411b9f4da89e
konumaru <konumaru1022@gmail.com>
2023-01-25 01:13:13 +0900

    [add] pass model test

---
+++ tests/model/test_cvae.py
@@ -0,0 +1,41 @@
+import pytest
+import torch
+
+from src.model.cvae import CVAELoss, CVAEModel
+
+SMPL_DATA_SIZE = 16
+
+
+@pytest.fixture
+def onehot_note_seq() -> torch.Tensor:
+    num_notes = 49
+    seq_len = 64
+
+    note_seq = torch.rand((SMPL_DATA_SIZE, seq_len, num_notes))
+    return note_seq
+
+
+@pytest.fixture
+def chromatic_chord_seq() -> torch.Tensor:
+    chord_dim = 12
+    seq_len = 64
+
+    chord_seq = torch.rand((SMPL_DATA_SIZE, seq_len, chord_dim))
+    return chord_seq
+
+
+def test_cvae_model(
+    onehot_note_seq: torch.Tensor, chromatic_chord_seq: torch.Tensor
+) -> None:
+    note_feat_dim = onehot_note_seq.shape[2]
+    chord_feat_dim = chromatic_chord_seq.shape[2]
+    model = CVAEModel(note_feat_dim, 16, 16, chord_feat_dim)
+    x_hat, mean, logvar = model.forward(onehot_note_seq, chromatic_chord_seq)
+
+    assert x_hat.permute(0, 2, 1).shape == onehot_note_seq.shape
+
+    label = onehot_note_seq.argmax(dim=2)
+
+    criterion = CVAELoss()
+    loss = criterion(label, x_hat, mean, logvar)
+    loss.backward()
